2fcf30f34fbb0727c5a0220aee88f260de0241d1
Roberto Pozzi <r.robipozzi@gmail.com>
2023-05-23 13:52:26 +0200

    refactorings

--- samples/nodejs/TemperatureSimulation/temperatureSimulationProducer.js
+++ samples/nodejs/TemperatureSimulation/temperatureSimulationProducer.js
@@ -16,7 +16,7 @@ if (KAFKA_ENVIRONMENT == "openshift") {
 }
 if (KAFKA_ENVIRONMENT == "confluent") {
   console.log("Use Kafka on Confluent");
-  kafka = require('./config/kafkaConfluentConfig___');
+  kafka = require('./config/kafkaConfluentConfig');
 }
 console.log("Kafka connection instantiated");
 // Creating Kafka Producer
